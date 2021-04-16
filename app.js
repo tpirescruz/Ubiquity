e97ac4fc13c9d00af6452a202b4051d07814e4ee
Tom <tpirescruz@gmail.com>
2021-04-16 12:15:33 +0200

    first slick slider + dots modifs

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-var left = document.getElementsByClassName("arrow-left");
-console.log(left);
-for (let i = 0; i < left.length; i++) {
-    const element = left[i];
-    element.addEventListener('click', function() {
-        console.log('I was clicked')
-    })
-    console.log(element)
+// var left = document.getElementsByClassName("arrow-left");
+// console.log(left);
+// for (let i = 0; i < left.length; i++) {
+//     const element = left[i];
+//     element.addEventListener('click', function() {
+//         console.log('I was clicked')
+//     })
+//     console.log(element)
 
-}
+// }
 
-var right = document.getElementsByClassName("arrow-right");
-console.log(right);
-for (let i = 0; i < right.length; i++) {
-    const element = right[i];
-    element.addEventListener('click', function() {
-        console.log('I was clicked');
-        target.classList.toggle('red')
-    })
+// var right = document.getElementsByClassName("arrow-right");
+// console.log(right);
+// for (let i = 0; i < right.length; i++) {
+//     const element = right[i];
+//     element.addEventListener('click', function() {
+//         console.log('I was clicked');
+//         target.classList.toggle('red')
+//     })
 
-};
-var target = document.getElementById("here");
+// };
+// var target = document.getElementById("here");
+
+
+
+$(document).ready(function() {
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow : $(".my-arrow-left"),
+        nextArrow : $(".my-arrow-right"),
+        dots: true,
+        dotClass: $(".slider-cube"),
+    });
+
+
+});
